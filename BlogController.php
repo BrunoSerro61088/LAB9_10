e1d8a29f4dbc6d6f3a9d599bc71115cce937ab67
BrunoSerro <55842349+BrunoSerro@users.noreply.github.com>
2020-12-17 17:36:14 +0000

    A

--- BlogController.php
+++ BlogController.php
@@ -28,13 +28,91 @@ class BlogController extends AbstractController
  	*/
  	public function index()
  	{
+ 		if ( $this->session->get('userid') )
+       	{
+           $data['posts'] = $this->blog_model->get_posts();
+           $data['menu0'] = "home";
+           $data['menu1'] = "logout";
+ 		   $data['menu2'] = "post";
+ 		   $data['welcome'] = "Welcome {{ username }}";
+           return $this->render('blog/index_template.html.twig', $data);   
+       	}
+
+       else
+       {         
+       	   $data['menu0'] = "home";
+           $data['menu1'] = "logout";
+           $data['menu2'] = "register";
+           return $this->render('blog/index_template.html.twig', $data);
+       }
+ 		/* LAB9
  		$data['posts'] = $this->blog_model->get_posts();
  		$data['menu0'] = "home";
  		$data['menu1'] = "logout";
  		$data['menu2'] = "post";
  		$data['welcome'] = "Welcome Bruno Serro";
 
- 		return $this->render('blog/index_template.html.twig', $data);
+ 		return $this->render('blog/index_template.html.twig', $data);*/
+ 	}
+
+ 	/**
+ 		* @Route("/blog/register", name="register")
+ 	*/ 
+ 	public function register()
+ 	{
+ 		if (  $this->session->get('userid') ) return $this->redirectToRoute('blog');
+    
+       	if ($request->isMethod('POST') && $request->attributes->get('_route') === 'register') 
+       	{
+        	// method is "POST": this is the "register action" controller part                 
+          	$token = $request->request->get("token");
+
+           	if (!$this->isCsrfTokenValid('register_form', $token)) 
+           	{
+            	return new Response("Operation not allowed", Response::HTTP_OK, ['content-type' => 'text/plain']);
+           	}
+      
+   			$username=$request->request->get('username');
+          	$email=$request->request->get('email');
+           	$password=$request->get('password');
+           	$passconf=$request->get('passconf');
+      
+      		$user = $this->blog_model->get_user($username);
+           	if ($user == false)
+               $value = '';
+           	else
+               $value = $user['username'];
+          
+           	$input = ['password' => $password, 'passconf' => $passconf, 'username' => $username, 'email' => $email];
+
+           	$constraints = new Assert\Collection([
+               'username' => [new Assert\NotBlank,
+                               new Assert\NotEqualTo(['value' => $value, 'message' => "This user is already in the database"])],
+               'email' => [new Assert\NotBlank,
+               				new Assert\NotEqualTo(['value' =>, 'message' => "This email is already in the database"])],
+               'password' => [new Assert\notBlank,
+                               new Assert\EqualTo(['value' => $passconf, 'message' => "Passwords do not match"])],
+               'passconf' => [new Assert\notBlank(['message' => "Password Confirmation must not be blank"])],             
+           ]);
+
+          
+           $data = $this->requestValidation($input, $constraints);
+              
+           if ( $data['errors'] > 0) {
+                   $data['username'] = $username;
+                   return $this->render('register.html.twig', $data);
+           }
+      
+           $this->blog_model->register_user($username,$email,$password);
+           return $this->redirectToRoute('blog');     
+       }
+       else {
+           // method is "GET": this is  the "register" controller part       
+           $data['errors'] = 0;
+           $data['username'] = '';
+           return $this->render('register.html.twig', $data);
+       }
  	}
 }
+
 ?>
--- Blog_modelController.php
+++ Blog_modelController.php
@@ -23,5 +23,21 @@ class Blog_modelController extends AbstractController
 		return $posts;
 
  	}
+
+    public function get_user($username)
+    {
+    	$sql = "SELECT * FROM users WHERE username='$username'";
+       	$stmt = $this->connection->prepare($sql);   
+        $stmt->execute();           
+        return $stmt->fetch();
+    }
+
+    public function register_user($username, $email, $password)
+   	{
+       $sql = "INSERT INTO users (username, email, password) VALUES ('" . $username . "','" . $email ."','" .substr(md5($password),0,32) . "')";
+       $stmt = $this->connection->prepare($sql);
+       $stmt->execute();   
+       return true;
+   	}
  }
  ?>
